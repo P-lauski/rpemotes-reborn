953c000d757740ca005e86d8de533ef38a71bfcf
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2026-07-30 18:38:54 -0400

    Export animal emote API
    Expose RP.AnimalEmotes and a model compatibility helper so other resources can reuse this resource's animal emote animations, useful for pet scripts

--- client/Emote.lua
+++ client/Emote.lua
@@ -911,6 +911,14 @@ CreateExport('getCurrentEmote', function()
     return currentEmote
 end)
 
+-- Expose the animal-emote list + model compatibility (useful for pet scripts)
+CreateExport('GetAnimalEmotes', function()
+    return RP.AnimalEmotes
+end)
+CreateExport('IsAnimalEmoteCompatible', function(model, emoteName)
+    return IsModelCompatible(model, emoteName)
+end)
+
 -- Door stuff
 local openingDoor = false
 AddEventHandler('CEventOpenDoor', function(unk1)
